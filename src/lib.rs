51b6ac1215d87e9db2591512a6f081224a817d78
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2023-11-08 10:04:58 +0300

    move InjectionConfig to the crate level

--- src/lib.rs
+++ src/lib.rs
@@ -10,4 +10,5 @@ pub use export_globals::export_mutable_globals;
 pub use parity_wasm;
 pub use stack_limiter::{
 	inject as inject_stack_limiter, inject_with_config as inject_stack_limiter_with_config,
+	InjectionConfig,
 };
